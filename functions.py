28abee16bc2687b18b27129d559e1cf096791df6
Oscar P <72569825+ng0-k1@users.noreply.github.com>
2025-03-01 22:43:36 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,6 +3,22 @@ from lxml import etree
 import requests
 
 def productos(productos):
+    """
+    Extrae información de productos desde MercadoLibre Colombia.
+
+    Args:
+        productos (str): Nombre del producto a buscar en la plataforma.
+
+    Returns:
+        tuple: Tres listas que contienen:
+            - lista_titulos (list): Títulos de los productos encontrados.
+            - lista_url (list): URLs de los productos.
+            - lista_precios (list): Precios de los productos.
+
+    Nota:
+        La función realiza web scraping sobre la plataforma de MercadoLibre para obtener la información.
+        Recorre múltiples páginas hasta alcanzar el final de la paginación.
+    """
     lista_titulos = []
     lista_url = []
     lista_precios = []
@@ -13,27 +29,27 @@ def productos(productos):
         r = requests.get(siguiente)
         if r.status_code == 200:
             soup = BeautifulSoup(r.content, 'html.parser')       
-            #Titulos
-            titulo = soup.find_all('h2',{'class':"ui-search-item__title"})
+            # Titulos
+            titulo = soup.find_all('h2', {'class': "ui-search-item__title"})
             titulo = [i.text for i in titulo]
             lista_titulos.extend(titulo)
-            #Url
-            url = soup.find_all('a', {'class' : "ui-search-item__group__element ui-search-link"})
+            # Url
+            url = soup.find_all('a', {'class': "ui-search-item__group__element ui-search-link"})
             url = [j.get('href') for j in url]
             lista_url.extend(url)
-            #Precios
+            # Precios
             dom = etree.HTML(str(soup))
             precios = dom.xpath('//ol/li/div/div/div[2]/div/div[1]/div[1]/div/div/div/span[1]/span[2]/span[2]')
             precios = [i.text for i in precios]
             lista_precios.extend(precios)
-            #inicial
-            inicio = soup.find('span', {'class':"andes-pagination__link"})
+            # Inicial
+            inicio = soup.find('span', {'class': "andes-pagination__link"})
             inicio = int(inicio.text)
-            #Fin
-            fin = soup.find('li', {'class':"andes-pagination__page-count"}).text
+            # Fin
+            fin = soup.find('li', {'class': "andes-pagination__page-count"}).text
             fin = int(fin.split(" ")[1])
         else:
-            print('No se puede acceder a la pagina socilitada')
+            print('No se puede acceder a la página solicitada')
             break
         print(inicio, fin)
         if inicio == fin:
@@ -42,6 +58,23 @@ def productos(productos):
     return lista_titulos, lista_url, lista_precios
 
 def limiteProducto(productos, limite):
+    """
+    Extrae información de productos desde MercadoLibre Colombia con un límite de resultados.
+
+    Args:
+        productos (str): Nombre del producto a buscar en la plataforma.
+        limite (int): Cantidad máxima de productos a obtener.
+
+    Returns:
+        tuple: Tres listas con un máximo de `limite` elementos cada una:
+            - lista_titulos (list): Títulos de los productos encontrados.
+            - lista_url (list): URLs de los productos.
+            - lista_precios (list): Precios de los productos.
+
+    Nota:
+        La función realiza web scraping en MercadoLibre y detiene la búsqueda cuando alcanza el número
+        especificado de productos o cuando no hay más páginas disponibles.
+    """
     lista_titulos = []
     lista_url = []
     lista_precios = []
@@ -52,44 +85,32 @@ def limiteProducto(productos, limite):
         r = requests.get(siguiente)
         if r.status_code == 200:
             soup = BeautifulSoup(r.content, 'html.parser')       
-            #Titulos
-            titulo = soup.find_all('h2',{'class':"ui-search-item__title"})
+            # Titulos
+            titulo = soup.find_all('h2', {'class': "ui-search-item__title"})
             titulo = [i.text for i in titulo]
             lista_titulos.extend(titulo)
-            #Url
-            url = soup.find_all('a', {'class' : "ui-search-item__group__element ui-search-link"})
+            # Url
+            url = soup.find_all('a', {'class': "ui-search-item__group__element ui-search-link"})
             url = [j.get('href') for j in url]
             lista_url.extend(url)
-            #Precios
+            # Precios
             dom = etree.HTML(str(soup))
             precios = dom.xpath('//ol/li/div/div/div[2]/div/div[1]/div[1]/div/div/div/span[1]/span[2]/span[2]')
             precios = [i.text for i in precios]
             lista_precios.extend(precios)
-            #inicial
-            inicio = soup.find('span', {'class':"andes-pagination__link"})
+            # Inicial
+            inicio = soup.find('span', {'class': "andes-pagination__link"})
             inicio = int(inicio.text)
-            #Fin
-            fin = soup.find('li', {'class':"andes-pagination__page-count"}).text
+            # Fin
+            fin = soup.find('li', {'class': "andes-pagination__page-count"}).text
             fin = int(fin.split(" ")[1])
         else:
-            print('No se puede acceder a la pagina socilitada')
+            print('No se puede acceder a la página solicitada')
             break
         print(inicio, fin)
-        if len(lista_titulos)>=int(limite):
+        if len(lista_titulos) >= int(limite):
             return lista_titulos[:limite], lista_url[:limite], lista_precios[:limite]
         if inicio == fin:
             break
         siguiente = dom.xpath('//div[@class="ui-search-pagination"]/ul/li[contains(@class, "button--next")]/a')[0].get('href')  
     return lista_titulos, lista_url, lista_precios
-
-
-
-
-
-
-
-
-
-
-
-
